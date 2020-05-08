099b1bbead7e008a9077fa254860bb72af9ba453
Ainara Vesga <ikawv@plaiaundi.net>
2020-05-08 10:53:44 +0200

    Agregado el jar file de SQLite en el proyecto

--- Protectora/src/capaDatos/Conexion.java
+++ Protectora/src/capaDatos/Conexion.java
@@ -1,12 +1,51 @@
 package capaDatos;
 
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Conexion {
 	
+	// Conexión a base de datos
+	private static Connection conectarA(String ruta) {
+		try {
+			// Cargamos el driver de SQLITE
+			Class.forName("org.sqlite.JDBC");
+		}
+		catch (ClassNotFoundException e){
+			// Si se produce un error con el driver de la BD
+			e.printStackTrace();
+		}
+		
+		// Declaramos la conexión
+		Connection conn = null;
+		try {
+			conn = DriverManager.getConnection("jdbc:sqlite:" + ruta);
+			// Escribimos mensaje en la consola para saber si
+			// la conexión se realizó y donde está el archivo
+			System.out.println("Conexión Ok - Ruta: " + ruta);						
+		}
+		catch (SQLException e) {
+			// Se ha producido un error en la BD
+			e.printStackTrace();
+		}
+		
+		//Devolvemos la conexión
+		return conn;
+	}
+	
 	public static void main(String[] args) {
-		// Prueba
-		System.out.println("Hola");
+		// Pasamos una ruta a la que nos vamos a conectar
+		Connection conn = conectarA("../../DatabaseProtectora/DatabaseProtectora.db");
+		
+		// Cerramos la conexión
+		try {
+			conn.close();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
